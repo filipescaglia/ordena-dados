94a679de44cf4ded7f1dd25ec1a3d368529e9a22
Filipe Scaglia <filipescaglia@gmail.com>
2019-02-18 19:21:58 -0300

    Projeto finalizado

--- OrdenaDados/Entities/Vector.cs
+++ OrdenaDados/Entities/Vector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 /*
  * Classe responsável pelo gerenciamento dos vetores.
  */
@@ -19,6 +20,32 @@ namespace OrdenaDados
             for (int i = 0; i < length; i++)
             {
                 v[i] = r.Next(0, 100000);
+                vO[i] = v[i];
+            }
+        }
+
+        //Método responsável pela criação dos vetores a partir de um arquivo de dados.
+        public static void FileVector(string source)
+        {
+            string[] tmp = null; //Vetor temporário para obtenção dos dados do arquivo.
+            try
+            {
+                foreach (string line in File.ReadAllLines(source))
+                {
+                    tmp = line.Split(','); //Separação dos valores pela vírgula.
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: " + e.Message);
+            }
+
+            v = new int[tmp.Length];
+            vO = new int[tmp.Length];
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                v[i] = int.Parse(tmp[i]);
                 vO[i] = v[i];
             }
         }
--- OrdenaDados/Program.cs
+++ OrdenaDados/Program.cs
@@ -14,19 +14,20 @@ namespace OrdenaDados
             do
             {
                 Console.WriteLine("::::::: MENU :::::::");
-                Console.WriteLine("1 - Gerar vetor");
-                Console.WriteLine("2 - Bubble sort");
-                Console.WriteLine("3 - Insertion sort");
-                Console.WriteLine("4 - Selection sort");
-                Console.WriteLine("5 - Quick sort");
-                Console.WriteLine("6 - Merge sort");
-                Console.WriteLine("7 - Shaker sort");
-                Console.WriteLine("8 - Heap sort");
-                Console.WriteLine("9 - Exibir vetor original");
-                Console.WriteLine("10 - Exibir vetor ordenado");
+                Console.WriteLine("1 - Gerar vetor com números aleatórios");
+                Console.WriteLine("2 - Gerar vetor a partir de um arquivo");
+                Console.WriteLine("3 - Bubble sort");
+                Console.WriteLine("4 - Insertion sort");
+                Console.WriteLine("5 - Selection sort");
+                Console.WriteLine("6 - Quick sort");
+                Console.WriteLine("7 - Merge sort");
+                Console.WriteLine("8 - Shaker sort");
+                Console.WriteLine("9 - Heap sort");
+                Console.WriteLine("10 - Exibir vetor original");
+                Console.WriteLine("11 - Exibir vetor ordenado");
                 Console.WriteLine("0 - Encerrar programa");
                 Console.WriteLine();
-                Console.Write("Escolha a opção desejada: ");
+                Console.Write("Digite a opção desejada: ");
                 menu = int.Parse(Console.ReadLine());
 
                 switch (menu)
@@ -39,6 +40,22 @@ namespace OrdenaDados
                         break;
 
                     case 2:
+                        Console.WriteLine();
+                        Console.WriteLine("::::::: INSTRUÇÕES PARA LEITURA DO ARQUIVO :::::::");
+                        Console.WriteLine("Por favor, certifique-se de que a pasta 'Files' contém o seu arquivo 'source.txt'" +
+                            " para que os dados sejam obtidos corretamente.");
+                        Console.WriteLine("Verifique também se os dados estão separados por uma ','.");
+                        Console.WriteLine("Exemplo:");
+                        Console.WriteLine("100,101,102,103...");
+                        Console.WriteLine("Após a verificação pressione ENTER.");
+                        Console.ReadLine();
+
+                        string sourceFilePath = @"..\..\..\Files\source.txt";
+                        Vector.FileVector(sourceFilePath);
+
+                        break;
+
+                    case 3:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Bubble.Sort(Vector.v);
@@ -47,7 +64,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 3:
+                    case 4:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Insertion.Sort(Vector.v);
@@ -56,7 +73,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 4:
+                    case 5:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Selection.Sort(Vector.v);
@@ -65,7 +82,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 5:
+                    case 6:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Quick.Sort(Vector.v, 0, Vector.v.Length - 1);
@@ -74,7 +91,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 6:
+                    case 7:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Merge.Sort(Vector.v, 0, Vector.v.Length - 1);
@@ -83,7 +100,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 7:
+                    case 8:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Shaker.Sort(Vector.v);
@@ -92,7 +109,7 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 8:
+                    case 9:
                         Vector.CopyVector();
                         start = DateTime.Now;
                         Heap.Sort(Vector.v);
@@ -101,12 +118,12 @@ namespace OrdenaDados
                         Time.CalcTime(start, end);
                         break;
 
-                    case 9:
+                    case 10:
                         Vector.ShowOriginalVector();
                         Console.WriteLine();
                         break;
 
-                    case 10:
+                    case 11:
                         Vector.ShowSortedVector();
                         Console.WriteLine();
                         break;
